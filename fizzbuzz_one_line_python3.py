4aa100652e1e43b5b06163dd70eba04d39dc2d19
GB0x <root@localhost>
2020-02-26 19:17:45 -0500

    Added a meaningful comment

--- fizzbuzz_one_line_python3.py
+++ fizzbuzz_one_line_python3.py
@@ -1,2 +1,3 @@
+# Wasted few minutes to solve this challenge. Such a waste of time :(.
 # one line python3 fizz buzz (with no extra lines). 
 [print(str(i)+":" +(i%3==0)*"Fizz"+(i%5==0)*"Buzz") for i in range(0, 101) if ((i%3==0) or (i%5==0))]
