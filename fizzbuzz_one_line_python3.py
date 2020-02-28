2cb72a3ff2ff85c220250fd3b659924e95f43a43
0x4F6D616E <omanipython@gmail.com>
2020-02-28 10:30:55 -0500

    Update fizzbuzz_one_line_python3.py

--- fizzbuzz_one_line_python3.py
+++ fizzbuzz_one_line_python3.py
@@ -1,3 +1,10 @@
 # Wasted few minutes to solve this challenge. Such a waste of time :(.
 # one line python3 fizz buzz (with no extra lines). 
 [print(str(i)+":" +(i%3==0)*"Fizz"+(i%5==0)*"Buzz") for i in range(0, 101) if ((i%3==0) or (i%5==0))]
+
+# Using lambda showing all numbers from 0-100
+for x in list((map(lambda i: (str(i)+":"+"Fizz"*(i%3==0)+"Buzz"*(i%5==0)), range(0,101)))): print(x)
+# ^ same but without redundancy(non-fizz-buzz numbers are omited through the built-in filter function):
+for x in list(filter(None ,map(lambda i: ((str(i)+":"+"Fizz"*(i%3==0)+"Buzz"*(i%5==0))*(i%3 == 0 or i%5==0)), range(0,101)))): print(x)
+# If you want the result without the leading number that was tested, just remote str(i)+":", and you should be good. It was added
+# just for clarification purposes.
